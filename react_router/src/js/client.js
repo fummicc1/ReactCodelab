3ffb8975a816a1b80bc8e89fd6fcb5aedb6375ca
fummicc1 <fumiya989@gmail.com>
2020-04-01 07:57:56 +0900

    add Pages

--- react_router/src/js/client.js
+++ react_router/src/js/client.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './pages/layout'
+import Featured from './pages/featured';
+import Settings from './pages/settings';
+import Archives from './pages/archives';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const app = document.getElementById("app");
-ReactDOM.render(<Layout/>, app);
+
+ReactDOM.render(
+    <Router>
+        <Layout>
+            <Route exact path="/" component={Featured}></Route>
+            <Route path="/archives" component={Archives}></Route>
+            <Route path="/settings" component={Settings}></Route>
+        </Layout>
+    </Router>,
+    app
+);
