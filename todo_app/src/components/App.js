fbc7b66017bf90eff2d7b58f08abd5b310245f12
fummicc1 <fumiya.tennis1@gmail.com>
2020-04-04 22:01:52 +0900

    setup

---
+++ todo_app/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="siimple-box siimple--bg-dark">
+        <h1 className="siimple-box-title siimple--color-white">ToDoApp</h1>
+      <Form></Form>
+      <Form></Form>
+      <Form></Form>
+      <Form></Form>
+      <Form></Form>
+      <Form></Form>
+      <Form></Form>
+      </div>      
+    </div>
+  );
+}
+
+export default App;
