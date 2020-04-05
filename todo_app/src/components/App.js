4ed10c4d348789196367769fdc5b69ad645f2eba
fummicc1 <fumiya.tennis1@gmail.com>
2020-04-05 21:17:38 +0900

    add List.js

--- todo_app/src/components/App.js
+++ todo_app/src/components/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import Form from './Form';
+import List from './List';
 
 function App() {
   return (
     <div className="App">
       <div className="siimple-box siimple--bg-dark">
         <h1 className="siimple-box-title siimple--color-white">ToDoApp</h1>
-      <Form></Form>
-      <Form></Form>
-      <Form></Form>
-      <Form></Form>
-      <Form></Form>
-      <Form></Form>
-      <Form></Form>
-      </div>      
+      <Form/>
+      <div className="siimple-rule"></div>
+      <List />
+      </div>
     </div>
   );
 }
