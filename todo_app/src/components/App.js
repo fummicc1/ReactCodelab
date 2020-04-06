24d5345b14c05052dc20f1b49b6db6d29eea6c69
fummicc1 <fumiya.tennis1@gmail.com>
2020-04-06 14:59:39 +0900

    delete siimple

--- todo_app/src/components/App.js
+++ todo_app/src/components/App.js
@@ -5,10 +5,10 @@ import List from './List';
 function App() {
   return (
     <div className="App">
-      <div className="siimple-box siimple--bg-dark">
-        <h1 className="siimple-box-title siimple--color-white">ToDoApp</h1>
+      <div className="">
+        <h1 className="">ToDoApp</h1>
       <Form/>
-      <div className="siimple-rule"></div>
+      <div className=""></div>
       <List />
       </div>
     </div>
--- todo_app/src/components/Form.js
+++ todo_app/src/components/Form.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Form(props) {
     return (
-        <form className="siimple-form">
-            <div className="siimple-form-field">
-                <label className="siimple-label siimple--color-white">ToDo Content: </label>
-                <input name="title" type="text" className="siimple-input"></input>
-                <input type="submit" value="Add" className="siimple-btn siimple-btn--teal"></input>
+        <form className="">
+            <div className="">
+                <label className="">ToDo Content: </label>
+                <input name="title" type="text" className=""></input>
+                <input type="submit" value="Add" className=""></input>
             </div>
         </form>
     )
