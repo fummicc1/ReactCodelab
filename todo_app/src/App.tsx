5b5284abe1f627323cb2f50809e8b3aab84ec393
Fumiya Tanaka <fumiyatanaka@FumiyanoMacBook-puro.local>
2020-04-12 11:09:36 +0900

    fix user state

--- todo_app/src/App.tsx
+++ todo_app/src/App.tsx
@@ -2,43 +2,58 @@ import React, { ReactNode } from 'react'
 import './App.css'
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import firebase from 'firebase'
+import firebase, { User } from 'firebase'
 import SignUp from './pages/SignUp'
 import ToDo from './pages/ToDo'
 import { firebaseConfig } from './firebase/config'
+import ToDoList from './pages/ToDoList'
 
 
 const App: React.FC = () => {
   firebase.initializeApp(firebaseConfig)
-  if (firebase.auth().currentUser == null) {
-    return (
-      <Home child={<SignUp></SignUp>}>
-      </Home>
-    )
-  }
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
   return (
-    <Home child={<ToDo></ToDo>}>
+    <Home>
     </Home>
   )
 }
 
-interface Props {
-  child: ReactNode
+interface Props {}
+interface State {
+  isSignin: boolean
 }
 
-const Home: React.FC<Props> = (props) => {
-  return (
-    <div className="App">
-      <Container>  
-        <Jumbotron fluid>
-          <h1>
-            ToDo App
-          </h1>
-        </Jumbotron>     
-        {props.child}
-      </Container>
-    </div>
-  )
+class Home extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      isSignin: firebase.auth().currentUser == null
+    }
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+  }
+
+  onAuthStateChanged = (user: firebase.User | null) => {
+    this.setState({
+      isSignin: user == null
+    })
+  }
+
+  render() {
+    const body: ReactNode = this.state.isSignin ? <ToDoList/> : <SignUp />
+    return (
+      <div className="App">
+        <Container>  
+          <Jumbotron fluid>
+            <h1>
+              ToDo App
+            </h1>
+          </Jumbotron>     
+          {body}
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App
