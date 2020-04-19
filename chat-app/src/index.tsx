fd453d03338cf7e3f3cc6d06935fc34285741df3
fummicc1 <fumiya.tennis1@gmail.com>
2020-04-19 09:10:31 +0900

    add: initialize firebase.

--- chat-app/src/index.tsx
+++ chat-app/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase/app';
+import { firebaseConfig } from './../firebase-config';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
